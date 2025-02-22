ec1cd89da5ed3f7ee06021eda84ea299790e53bf
HARSHIT-3112 <chaturvedih45@gmail.com>
2025-02-23 03:02:16 +0530

    Updated DSA solutions - 2025-02-23

--- hashing/basic_hash.cpp
+++ hashing/basic_hash.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter the number of elements: ";
     cin>>n;
 
     int arr[n];
+    cout<<"Enter the elements: ";
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
@@ -16,10 +18,13 @@ int main(){
     }
 
     int q; 
+    cout<<"Enter the number of queries: ";
     cin>>q;
     while(q--){
         int x;
+        cout<<"Enter the element to be searched: ";
         cin>>x;
+
         cout<<hash[x]<<endl;
     }
     return 0;
