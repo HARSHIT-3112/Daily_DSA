e90af93210fe222058895894489879d8ba62a958
HARSHIT-3112 <chaturvedih45@gmail.com>
2025-03-06 22:00:27 +0530

    Updated DSA solutions - 2025-03-06

--- bit_manipulation/binaryc_conversion.cpp
+++ bit_manipulation/binaryc_conversion.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 string decimaltobinary(int n){
-    string s ="";
+    string str =""; // to store the binary representation
     while(n>0){
-        s+=to_string(n%2); // this will append all the bit but in reverse order
+        str+=to_string(n%2); // this will append all the bit but in reverse order
         n = n/2;
     }
-    reverse(s.begin(), s.end()); // reversing the string to get the correct binary representation
-    return s;   
+    reverse(str.begin(), str.end()); // reversing the string to get the correct binary representation
+    return str;   
 }
 
 int main(){
